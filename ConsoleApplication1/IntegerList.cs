8ffa85ac62b5321632643b175f31ff9f76cc98df
hrga\Luka Hrgović <luka.hrgovic@fer.hr>
2016-10-20 00:06:20 +0200

    ZADATAK1

---
+++ ConsoleApplication1/IntegerList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    public class IntegerList : IIntegerList
+    {
+        public int[] _internalStorage;
+        private int initialSize = 4;
+        private int size = 0;
+        private int numOfItems = 0;
+
+        public IntegerList()
+        {
+            _internalStorage = new int[initialSize];
+            size = initialSize;
+        }
+       
+        public IntegerList(int initialSize)
+        {
+            if (initialSize < 0)
+                throw new ArgumentException("Krivi unos podataka!");
+            else
+            {
+                this.initialSize = initialSize;
+                size = initialSize;
+                _internalStorage = new int[this.initialSize];
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return numOfItems;
+            }
+        }
+
+        public void Add(int item)
+        {
+            if (numOfItems == size)
+            {
+                Array.Resize(ref _internalStorage, 2*size);
+                size *= 2;
+            }
+            _internalStorage[numOfItems] = item;
+            numOfItems++;
+        }
+
+        public bool Remove(int item)
+        {
+            int position = 0;
+            while (_internalStorage[position] != item)
+            {
+                position++;
+                if (position == numOfItems)
+                    return false;
+            }
+            return RemoveAt(position);
+        }
+
+        public bool RemoveAt(int index)
+        {
+            if (index >= numOfItems)
+                return false;
+            for(int i=index; i<numOfItems; i++)
+            {
+                if (index < numOfItems - 1)
+                    _internalStorage[i] = _internalStorage[i + 1];
+            }
+            numOfItems--;
+            return true;
+        }
+
+        public int GetElement(int index)
+        {
+            if (index < numOfItems)
+                return _internalStorage[index];
+            else
+                throw new IndexOutOfRangeException();
+        }
+
+        public int IndexOf(int item)
+        {
+            for (int i = 0; i < numOfItems; i++)
+                if (_internalStorage[i] == item)
+                    return i;                  
+            throw new ArgumentException("ne postoji takav element u spremniku!");
+        }
+
+        public void Clear()
+        {
+            Array.Resize(ref _internalStorage, 0);
+            numOfItems = 0;
+        }
+
+        public bool Contains(int item)
+        {
+            foreach (int index in _internalStorage)
+                if (index == item)
+                    return true;
+            return false;
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            IntegerList listOfIntegers = new IntegerList();
+            ListExample(listOfIntegers);
+        }
+
+        public static void ListExample(IntegerList listOfIntegers)
+        {
+            listOfIntegers.Add(1); // [1]
+            Console.WriteLine(listOfIntegers.Count);
+            Console.ReadKey();
+            listOfIntegers.Add(2); // [1 ,2]
+            Console.WriteLine(listOfIntegers.Count);
+            Console.ReadKey();
+            listOfIntegers.Add(3); // [1 ,2 ,3]
+            Console.WriteLine(listOfIntegers.Count);
+            Console.ReadKey();
+            listOfIntegers.Add(4); // [1 ,2 ,3 ,4]
+            Console.WriteLine(listOfIntegers.Count);
+            Console.ReadKey();
+            listOfIntegers.Add(5); // [1 ,2 ,3 ,4 ,5]
+            Console.WriteLine(listOfIntegers.Count);
+            Console.WriteLine(listOfIntegers._internalStorage[4]);
+            Console.ReadKey();
+            listOfIntegers.RemoveAt(0); // [2 ,3 ,4 ,5]
+            Console.WriteLine(listOfIntegers.Count);
+            Console.WriteLine(listOfIntegers._internalStorage[4]);
+            Console.ReadKey();
+            listOfIntegers.Remove(5); // [2 ,3 ,4];
+            Console.WriteLine(listOfIntegers._internalStorage[4]);
+            Console.WriteLine(listOfIntegers.Count); // 3
+            Console.ReadKey();
+
+            Console.WriteLine(listOfIntegers.Remove(100)); // false
+            Console.WriteLine(listOfIntegers.RemoveAt(5)); // false
+            listOfIntegers.Clear(); // []
+            Console.WriteLine(listOfIntegers.Count); // 0
+            Console.ReadKey();
         }
     }
-}
+}        
+
