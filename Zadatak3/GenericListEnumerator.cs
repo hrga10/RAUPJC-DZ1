4464ba29de1c257b0573c3ca12237b427ec54df0
hrga\Luka Hrgović <luka.hrgovic@fer.hr>
2016-10-20 15:55:59 +0200

    ZADATAK3

---
+++ Zadatak3/GenericListEnumerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Zadatak3
+{
+    public class GenericListEnumerator<X> : IEnumerator<X>
+    {
+        private GenericList<X> genericList;
+        private int position = -1;
+
+        public GenericListEnumerator(GenericList<X> genericList)
+        {
+            this.genericList = genericList;
+        }
+
+        public X Current
+        {
+            get
+            {
+                try
+                {
+                    return genericList._internalStorage[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < genericList.numOfItems);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
---
+++ Zadatak3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            list.Add(1);
+            list.Add(4);
+            Console.WriteLine(list.Count);
+            Console.ReadKey();
+            
+            foreach (int value in list)
+            {
+                Console.WriteLine(value);
+            }
+            
+        }
+    }
+}
